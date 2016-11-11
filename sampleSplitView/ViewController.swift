367adf846e9b664a8f6eef6fce0d2a63acdd0b56
gaku <gaku41meric706@gmail.com>
2016-11-11 11:38:49 +0900

    Initial Commit

---
+++ sampleSplitView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleSplitView
+//
+//  Created by 木下岳 on 2016/11/11.
+//  Copyright © 2016年 gakukinoshita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
